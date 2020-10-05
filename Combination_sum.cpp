31e101875827153948b9787e02b9e80118b5d1c0
Siddhant829 <71970270+Siddhant829@users.noreply.github.com>
2020-10-05 23:58:28 +0530

    Update Combination_sum.cpp

--- Combination_sum.cpp
+++ Combination_sum.cpp
@@ -8,12 +8,12 @@ It is guaranteed that the number of unique combinations that sum up to target is
 
 Example 1:
 
-Input: candidates = [2,3,6,7], target = 7
+Input: candidates = [2,3,6,7],target = 7
 Output: [[2,2,3],[7]]
-Explanation:
+Explanation: 
 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
 7 is a candidate, and 7 = 7.
-These are the only two combinations.
+These are the only two combinations. 
 Example 2:
 
 Input: candidates = [2,3,5], target = 8
@@ -21,7 +21,7 @@ Output: [[2,2,2,2],[2,3,3],[3,5]]
 Example 3:
 
 Input: candidates = [2], target = 1
-Output: []
+Output: [] 
 Example 4:
 
 Input: candidates = [1], target = 1
